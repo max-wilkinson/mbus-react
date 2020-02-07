385eb8df3ef972502cdb399baf0c9951562d6456
Max Wilkinson <mawilkin@umich.edu>
2020-02-07 10:47:26 -0500

    Created skeleton react app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleText = getByText(/MBus/i);
+  expect(titleText).toBeInTheDocument();
 });
