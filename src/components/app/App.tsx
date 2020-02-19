35861813ea225d13d5e7ba47c24bc8bf893ecfaa
Max Wilkinson <mawilkin@umich.edu>
2020-02-19 15:08:02 -0500

    Added react hook to GraphQL

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import './App.scss';
 import { Card } from '../card/Card';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'https://mbusql.azurewebsites.net/graphql'
+});
 
 const App = () => {
   return (
-    <div className="app">
-      <header>
-        <h1 className="title">MBus</h1>
-      </header>
-      <div className="cardList">
-        <Card></Card>
+    <ApolloProvider client={client}>
+      <div className="app">
+        <header>
+          <h1 className="title">MBus</h1>
+        </header>
+        <div className="cardList">
+          <Card stopId={137}></Card>
+        </div>
       </div>
-    </div>
+    </ApolloProvider>
   );
 };
 
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 import './Card.scss';
 
-export const Card = () => {
+interface Props {
+  stopId: number;
+}
+
+const ROUTES = gql`
+  {
+    stops {
+      name
+    }
+  }
+`;
+
+export const Card = (props: Props) => {
+  const { loading, error, data } = useQuery(ROUTES);
+
+  if (loading) return <div></div>;
+  else if (error) return <div></div>;
+
   return (
     <div className="card">
       <div className="accentLine"></div>
-      <h2 className="busStopText">Pierpont Commons</h2>
+      <h2 className="busStopText">{data.stops[0].name}</h2>
       <div className="routeDetails">
         <h3 className="routeName">Commuter South</h3>
         <h4 className="routeStatus">5 minutes until next bus</h4>
