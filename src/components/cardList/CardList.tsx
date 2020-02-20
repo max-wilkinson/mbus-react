248e5c3a6f4547027edf3f6e0882c38bc33b1c6f
Max Wilkinson <mawilkin@umich.edu>
2020-02-20 14:30:45 -0500

    Used location service to render nearby stops

--- src/components/cardList/CardList.tsx
+++ src/components/cardList/CardList.tsx
@@ -1,31 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
+import { LocationService } from '../../services/LocationService';
 import { Card } from '../card/Card';
-import './CardList.scss';
 import Stop from '../../models/Stop';
+import './CardList.scss';
 
 const STOPS = gql`
   {
     activeStops {
       id
       name
+      lat
+      lon
     }
   }
 `;
 
 export const CardList = () => {
+  const [nearbyStops, setNearbyStops] = useState<Stop[]>([]);
   const { loading, error, data } = useQuery(STOPS);
+
+  const locationService = new LocationService();
+
+  useEffect(() => {
+    async function determineNearbyStops() {
+      setNearbyStops(
+        await locationService.determineNearbyStops(data.activeStops)
+      );
+    }
+
+    if (data !== undefined) {
+      determineNearbyStops();
+    }
+  });
 
   if (loading) return <div></div>;
   else if (error) return <div></div>;
 
   return (
     <div>
-      {data.activeStops.slice(0, 3).map((stop: Stop, i: number) => {
+      {nearbyStops.slice(0, 5).map((stop: Stop, i: number) => {
         return (
-          <div className="card">
-            <Card stop={stop} key={i}></Card>
+          <div className="card" key={i}>
+            <Card stop={stop}></Card>
           </div>
         );
       })}
