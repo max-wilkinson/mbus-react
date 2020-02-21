7f2483782ac9801ca7a25a2c2042b399c9ccaa78
Max Wilkinson <mawilkin@umich.edu>
2020-02-21 11:34:07 -0500

    Added logic to render all ETAs and Routes for each stop

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
 import Stop from '../../models/Stop';
@@ -23,9 +23,29 @@ const ETAROUTES = gql`
 `;
 
 export const Card = (props: Props) => {
+  let defaultEtas: { [id: string]: number[] } = {};
+  const [etasByRoute, setEtasByRoute] = useState(defaultEtas);
   const { loading, error, data } = useQuery(ETAROUTES, {
     variables: { stop: Number(props.stop.id) }
   });
+
+  useEffect(() => {
+    if (data === undefined) {
+      return;
+    }
+
+    const combinedEtas: any = {};
+
+    for (const eta of data.etas) {
+      if (eta.route in combinedEtas) {
+        combinedEtas[eta.route].push(eta.avg);
+      } else {
+        combinedEtas[eta.route] = [eta.avg];
+      }
+    }
+
+    setEtasByRoute(combinedEtas);
+  }, [props.stop, data]);
 
   if (loading) return <div></div>;
   else if (error) return <div></div>;
@@ -34,12 +54,29 @@ export const Card = (props: Props) => {
     <div className="card">
       <div className="accentLine"></div>
       <h2 className="busStopText">{props.stop.name}</h2>
-      <div className="routeDetails">
-        <h3 className="routeName">{data.routes[0].name}</h3>
-        <h4 className="routeStatus">
-          {data.etas[0].avg} minutes until next bus
-        </h4>
-      </div>
+      {Object.keys(etasByRoute).map((route: string, i: number) => {
+        return (
+          <div className="routeDetails" key={i}>
+            <h3 className="routeName">{getRouteName(route, data.routes)}</h3>
+            {Object.values(etasByRoute[route]).map((eta: any, i: number) => {
+              return (
+                <h4 className="routeStatus" key={i}>
+                  {eta} minutes until next bus
+                </h4>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 };
+
+function getRouteName(routeId: string, routes: any): String {
+  for (const route of routes) {
+    if (route.id === routeId) {
+      return route.name;
+    }
+  }
+  return 'Unknown Route';
+}
