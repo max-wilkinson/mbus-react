3401abfd2c75b2c9103c3b1638cd3c9fb35f5b06
Max Wilkinson <mawilkin@umich.edu>
2020-02-24 10:11:42 -0500

    Added accent bars

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import './App.scss';
 import { CardList } from '../cardList/CardList';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
+import accentBars from '../../assets/accentBars.svg';
+import './App.scss';
 
 const client = new ApolloClient({
   uri: 'https://mbusql.azurewebsites.net/graphql'
@@ -12,12 +13,14 @@ const App = () => {
   return (
     <ApolloProvider client={client}>
       <div className="app">
+        <img src={accentBars} className="accentBars topBars" alt="" />
         <header>
           <h1 className="title">MBus</h1>
         </header>
         <div className="cardList">
           <CardList></CardList>
         </div>
+        <img src={accentBars} className="accentBars bottomBars" alt="" />
       </div>
     </ApolloProvider>
   );
