f6a8093249a57c4839a300a6e3b5a844f419de4f
Max Wilkinson <mawilkin@umich.edu>
2020-02-24 13:14:30 -0500

    Updates

---
+++ precache-manifest.91a75236fed60788fb7afaf33ddcc354.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a5cb98c605fdcbd214c1186a4497138",
+    "url": "/mbus-react/index.html"
+  },
+  {
+    "revision": "e7f86d3238f9aec4129d",
+    "url": "/mbus-react/static/css/main.9fa75414.chunk.css"
+  },
+  {
+    "revision": "62b148b03c6ec92cba91",
+    "url": "/mbus-react/static/js/2.cf19241f.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/mbus-react/static/js/2.cf19241f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7f86d3238f9aec4129d",
+    "url": "/mbus-react/static/js/main.e9d807c6.chunk.js"
+  },
+  {
+    "revision": "072a836db0d3496bc0eb",
+    "url": "/mbus-react/static/js/runtime-main.88296aba.js"
+  },
+  {
+    "revision": "50ec595ec2246ed7af60c51016f8ce56",
+    "url": "/mbus-react/static/media/accentBars.50ec595e.svg"
+  }
+]);
